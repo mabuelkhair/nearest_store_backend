3556f43923e827f7bf7de0a35cace61d0c818df5
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-10-07 16:41:06 +0200

    add more tests to login (wrong data and missing data)

--- accounts/tests.py
+++ accounts/tests.py
@@ -144,3 +144,22 @@ class AccountsTest(APITestCase):
         user = User.objects.get(username='testuser')
         token = Token.objects.get(user=user)
         self.assertEqual(response.data['token'], token.key)
+
+    def test_login_user_with_wrong_data(self):
+
+        data = {
+            'username': 'testuser',
+            'password': 'wrongpass'
+        }
+
+        response = self.client.post(self.login_url , data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_user_with_missing_data(self):
+
+        data = {
+            'username': 'testuser'
+        }
+
+        response = self.client.post(self.login_url , data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
