f9f2641d0588e14c54287430bc7f44380693b0bf
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-10-07 14:59:25 +0200

    add accounts urls and create dummy view to test urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from accounts.serializers import UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserCreate(APIView):
+
+    def post(self, request, format='json'):
+        return Response('hello')
