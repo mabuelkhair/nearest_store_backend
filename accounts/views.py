2024552b41ee29a16d8240df5f2d0088af0e12b7
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-10-07 15:42:29 +0200

    create token to user on registeration and return it in json

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from accounts.serializers import UserSerializer
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token
 
 class UserCreate(APIView):
 
@@ -11,5 +13,8 @@ class UserCreate(APIView):
         if serializer.is_valid():
             user = serializer.save()
             if user:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                token = Token.objects.create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
