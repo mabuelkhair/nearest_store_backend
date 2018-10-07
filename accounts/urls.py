ad2035c2b5809b91bed5c8dc7fcae3100d2ebc02
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-10-07 21:07:52 +0200

    add logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from rest_framework.authtoken import views as rest_views
 urlpatterns = [
     path('api/users/register', views.UserCreate.as_view(), name='register'),
     path('api/users/login', views.Login.as_view(), name='login'),
+    path('api/users/logout', views.Logout.as_view(), name='logout'),
 ]
