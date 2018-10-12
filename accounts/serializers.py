2f219a5eda398fc94c8d00eea39df194eeb88adc
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-10-12 18:56:30 +0200

    create profile serializer and add it to userserialzer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from .models import Profile
+from django.contrib.gis.geos import Point
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('location',)
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,16 +23,21 @@ class UserSerializer(serializers.ModelSerializer):
 
     )
     password = serializers.CharField(min_length=8, write_only=True)
+    profile = ProfileSerializer(required=True)
 
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'],
                                         validated_data['email'],
                                         validated_data['password'])
+        profile_data = validated_data.pop('profile')
+        location = Point(x=float(profile_data['location']['lat']),
+                         y=float(profile_data['location']['lon']), srid=4326)
+        Profile.objects.create(user=user, location=location)
         return user
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password', 'profile',)
 
 
 class LoginSerializer(serializers.Serializer):
