0a9701809935639d12426190904adc51994a2678
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-10-13 21:40:22 +0200

    add name to store and generate migration file

--- stores/models.py
+++ stores/models.py
@@ -1,6 +1,7 @@
 from django.contrib.gis.db import models
 
 
-class Profile(models.Model):
+class Store(models.Model):
     location = models.PointField(null=False, blank=False,
                                  srid=4326, verbose_name="Location")
+    name = models.CharField(max_length=100)
